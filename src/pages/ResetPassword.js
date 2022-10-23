66ce4902441babb3a8a1ccdf2d347486229b6a10
MonikaPandian <panmonikmm@gmail.com>
2022-10-23 21:22:16 +0530

    inventory-billing-app

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,9 +1,43 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const ResetPassword = () => {
-
+    const[newPassword, setNewPassword] = useState('')
+    const[confirmPassword, setConfirmPassword] = useState('')
+    const { id, token} = useParams();
     const navigate = useNavigate();
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(newPassword !== confirmPassword){
+            window.alert("passwords does not match!!")
+            return;
+        }      
+        const userEmail = {
+            password: newPassword,                                      
+        };
+        fetch(`https://inventory-billing-121.herokuapp.com/users/reset-password/${id}/:${token}`, {
+            method: "POST",
+            body: JSON.stringify(userEmail),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+            .then((data) => data.json())
+            .then((data) => { 
+                if (data.message === "success") {
+                    window.alert("Password reset link has been sent to your mail. Please check your mail")
+                    navigate("/sign-in")                                                                    
+                }
+                else if(data.message === "Enter a valid and registered email Id"){
+                    window.alert("Enter a valid and registered email Id")
+                }
+                else{
+                    window.alert("something went wrong")
+                }
+            })
+    }
+
 
     return (
         <div>
@@ -18,20 +52,20 @@ const ResetPassword = () => {
             </nav>
             <div className="auth-wrapper">
                 <div className="auth-inner">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <h3>Reset Password</h3>
                         <div className="mb-3">
                             <label>New Password</label>
-                            <input
-                                type="password"
+                            <input value={newPassword}
+                                type="password" onChange={(e)=>setNewPassword(e.target.value)}
                                 className="form-control"
                                 placeholder="Enter new password" autoComplete='off'
                             />
                         </div>
                         <div className="mb-3">
                             <label>Confirm Password</label>
-                            <input
-                                type="password"
+                            <input value={confirmPassword}
+                                type="password"  onChange={(e)=>setConfirmPassword(e.target.value)}
                                 className="form-control"
                                 placeholder="Enter confirm password" autoComplete='off'
                             />
